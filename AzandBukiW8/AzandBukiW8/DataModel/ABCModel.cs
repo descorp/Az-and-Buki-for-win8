4bff2839bd633f66dbbdabb48f1c12a1385df5f7
ternista <kbelousova@tpu.ru>
2013-04-26 00:28:39 +0700

    Каркас приложений клиента и администратора (AdminApp)

---
+++ AzandBukiW8/AzandBukiW8/DataModel/ABCModel.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SQLite;
+
+namespace AzandBukiW8.Data
+{
+    //class ABCDataContext : DataContext
+    //{
+    //    public ABCDataContext() : base("Data Source=appdata:/ABCdb.sdf; File Mode=read only;") { }
+
+    //    public Table<Alphabet> Alphabets;
+    //    public Table<Letter> Letters;
+    //    public Table<Word> Words;
+    //}
+    
+
+    #region Alphabet
+
+    
+    public class Alphabet
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Language
+        {
+            get;
+            set;
+        }
+
+
+        public string Sprite
+        {
+            get;
+            set;
+        }
+
+    }
+    #endregion
+
+    #region Letter
+    public class Letter
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        
+        public int AlphabetID
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+    }
+    #endregion
+
+    #region Word
+    
+    public class Word
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public int LetterID
+        {
+            get;
+            set;
+        }
+
+        public int AlphabetID
+        {
+            get;
+            set;
+        }
+
+        public string Value
+        {
+            get;
+            set;
+        }
+
+        public string Sprite
+        {
+            get;
+            set;
+        }
+
+    }
+    #endregion
+
+}

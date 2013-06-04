9cf689cb56527b6924311b14127cb359697a6931
ternista <kbelousova@tpu.ru>
2013-06-04 15:58:04 +0700

    DBFiller added

---
+++ LevelUp!/App1/DataModel/DBFiller.cs
@@ -0,0 +1,56 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace levelupspace.DataModel
+{
+    public class DBFiller
+    {
+        public static async void LoadPackageToDB(String PathToPack, String DBPath)
+        {
+            
+            var folder = await StorageFolder.GetFolderFromPathAsync(PathToPack);
+            var file = await folder.GetFileAsync("letters.sql");
+            var LetterStrings = await FileIO.ReadLinesAsync(file);
+            
+            CreateDB(DBPath);
+            DBFiller.InsertStringsToDB(LetterStrings, DBPath);
+
+            file = await folder.GetFileAsync("words.sql");
+            var WordStrings = await FileIO.ReadLinesAsync(file);
+            DBFiller.InsertStringsToDB(WordStrings, DBPath);
+                      
+        }
+
+        private static void InsertStringsToDB(IList<string> ListString, String DBPath)
+        {
+            var db = new SQLiteConnection(DBPath);
+
+            for (int i = 0; i < ListString.Count; i++)
+            {
+                var command = db.CreateCommand(ListString[i]);
+                var result = command.ExecuteNonQuery();
+                if (result <= 0)
+                {
+                    //Какая-то ошибка
+                }
+            }
+        }
+
+        public static void CreateDB(String DBPath)
+        {
+            var db = new SQLiteConnection(DBPath);
+            db.CreateTable<Alphabet>();
+            db.CreateTable<Letter>();
+            db.CreateTable<AlphabetLocalization>();
+            db.CreateTable<Word>();
+            db.CreateTable<User>();
+            db.CreateTable<Award>();
+            db.CreateTable<AwardLocalization>();
+        }
+    }
+}
